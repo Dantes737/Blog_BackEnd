34f2daadb1f1ccadbfc925af4b7bdc2260d39fb6
Dantes737 <you@example.com>
2021-03-19 11:43:14 +0200

    cors

--- routes/users.js
+++ routes/users.js
@@ -45,4 +45,15 @@ router.get("/", function (req, res, next) {
 });
 
 
+router.get("/list", function (req, res, next) {
+  //Вибірка усіх документів з бази
+  User.find({}, function (err, docs) {
+    // mongoose.disconnect();
+    if (err) return res.status(500).json({ err: { msg: "Fetch faild!" } });
+
+    res.status(200).json({ success: true, users: docs });
+  });
+});
+
+
 module.exports = router;
