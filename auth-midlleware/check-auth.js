55ff9e0b81591d1a15d73b1a5bfd78cf4ff88287
Dantes737 <you@example.com>
2021-05-04 11:17:36 +0300

    auth-check update

--- auth-midlleware/check-auth.js
+++ auth-midlleware/check-auth.js
@@ -7,8 +7,8 @@ module.exports = (req, res, next) => {
     try {
         const AUTH_HEADER = req.headers['authorization'];
         const token = AUTH_HEADER && AUTH_HEADER.split(" ")[1];
-        console.log(token);
-        const decodet = jwt.verify(token, process.env.APP_TOKEN_SECRET);
+        // console.log(token);
+        const decodet = jwt.verify(token, process.env.SECRET_KEY);
         next();
         req.userData = decodet;
     } catch (error) {
